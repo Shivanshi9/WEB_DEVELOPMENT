cbedd5fd799bc94e0772a0ccc3ee47c676aac9c0
Shivanshi9 <shivanshisugandhi@gmail.com>
2026-04-30 18:26:14 +0530

    assignment-1 for loop

--- javascriptbasics/script.js
+++ javascriptbasics/script.js
@@ -1,10 +1,36 @@
-console.log("hello world");
-// js is a dynamic type language 
+// console.log("hello world");
+// // js is a dynamic type language 
 
-var x = 10;
-var x = 12;
-// let is used mostly
-console.log(x);
+// var x = 10;
+// var x = 12;
+// // let is used mostly
+// console.log(x);
+
+// for(var i=1; i<=10; i++)
+// {
+//     console.log(i*2);
+    
+// }
+
+// let a=[24,58,33,86,34,65,86,12,90,40]
+
+// for(let j=0; j<a.length ;j++)
+// {
+//     console.log(a[j]);   
+// }
+
+// let i=1;
+// while(i<=10)
+// {
+//     console.log(i*2);
+//     i++;
+// }
+
+let i=1;
+do{
+    console.log(i*2);
+    i++;   
+}while(i<=10);
 
 
 
