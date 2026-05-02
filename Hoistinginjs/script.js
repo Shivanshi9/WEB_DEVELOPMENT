ff7af32c68f2f8985ac5b76b9e1f03a338737e47
Shivanshi9 <shivanshisugandhi@gmail.com>
2026-05-02 19:33:47 +0530

    hoisting

---
+++ Hoistinginjs/script.js
@@ -0,0 +1,21 @@
+//rule of hoisting is to declare all the variables at the top of code
+let a;
+let b ;
+let c ;
+
+a= 10;
+console.log(a); // 10
+console.log(b); // undefined 
+console.log(c); // undefined
+
+ b = 20;
+
+console.log(a); // 10
+console.log(b); // 20 
+console.log(c);  // undefind 
+
+ c= 20;
+
+console.log(a);
+console.log(b);
+console.log(c);   
