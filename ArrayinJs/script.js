91c3ebebe647bc8fba3b951866a82dcfc991ccd0
Shivanshi9 <shivanshisugandhi@gmail.com>
2026-05-05 19:13:44 +0530

    CRUD operation on array in js

---
+++ ArrayinJs/script.js
@@ -0,0 +1,11 @@
+let arr = [10,11,12,13,15]
+
+console.log(arr);
+
+arr[2] = arr[2]+3
+console.log(arr);
+
+arr =[]
+console.log(arr);
+
+
--- JsDOM/script.js
+++ JsDOM/script.js
@@ -1,10 +1,32 @@
-function Submit(){
-    // console.log("Submit button clicked");
+function Submit() {
 
-    const fn = document.getElementById("fullName").value;
-    console.log(fn);
-    
-    document.getElementById("myData").innerHTML=fn;
-    document.getElementById("fullName").value="";
+  console.log("Submit Button Click");
 
+  // get values
+  const name = document.getElementById("fullName").value;
+  const age = document.getElementById("age").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  // console output
+  console.log("Name :", name);
+  console.log("Age :", age);
+  console.log("Email :", email);
+  console.log("Password :", password);
+
+  // show data on webpage
+  document.getElementById("myData").innerHTML =
+    "<p><b>Name :</b> " + name + "</p>" +
+    "<p><b>Age :</b> " + age + "</p>" +
+    "<p><b>Email :</b> " + email + "</p>" +
+    "<p><b>Password :</b> " + password + "</p>";
+
+  // clear input fields
+  document.getElementById("fullName").value = "";
+  document.getElementById("age").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
+
+  document.getElementById("datacard").classList.add("displayshow")
+  document.getElementById("datacard").classList.remove("displayhide")
 }
