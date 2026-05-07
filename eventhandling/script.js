f7ee5b67f08e591895263e16ed446fd73eb9600e
Shivanshi9 <shivanshisugandhi@gmail.com>
2026-05-07 19:21:00 +0530

    event handling

---
+++ eventhandling/script.js
@@ -0,0 +1,19 @@
+function Bulbon() {
+    document.getElementById("bulb1").style.backgroundColor = "yellow";
+}
+
+function BulbOff() {
+    document.getElementById("bulb1").style.backgroundColor = "white";
+}
+
+function Bulbred() {
+    document.getElementById("bulb1").style.backgroundColor = "red";
+}
+
+function Bulblue() {
+    document.getElementById("bulb1").style.backgroundColor = "blue";
+}
+
+function Bulbgreen() {
+    document.getElementById("bulb1").style.backgroundColor = "green";
+}
